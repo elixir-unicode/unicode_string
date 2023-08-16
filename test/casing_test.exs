893a4bbcd552e04ee1b40c4dcae2c93b6fb87dbf
Kip Cole <kipcole9@gmail.com>
2023-08-16 09:37:22 +0800

    Additional tr/az casing tests

--- test/casing_test.exs
+++ test/casing_test.exs
@@ -1,16 +1,24 @@
 defmodule UnicodeString.Casing.Test do
   use ExUnit.Case, async: true
 
-  test "Casing greek with final sigma" do
+  test "Casing Greek with final sigma" do
     assert Unicode.String.Case.Mapping.upcase("Ὀδυσσεύς") == "ὈΔΥΣΣΕΎΣ"
     assert Unicode.String.Case.Mapping.titlecase("ὈΔΥΣΣΕΎΣ") == "Ὀδυσσεύς"
     assert Unicode.String.Case.Mapping.downcase("ὈΔΥΣΣΕΎΣ") == "ὀδυσσεύς"
   end
 
-  test "Upcasing i in the Turkish and Azerbaijani locales returns capital dotted-I" do
+  test "That accents are removed when upcasing Greek" do
+    string = "Πατάτα, Αέρας, Μυστήριο, Ωραίο, Μαΐου, Πόρος, Ρύθμιση, ΰ, Τηρώ, Μάιος, άυλο"
+    upcased = "ΠΑΤΑΤΑ, ΑΕΡΑΣ, ΜΥΣΤΗΡΙΟ, ΩΡΑΙΟ, ΜΑΙΟΥ, ΠΟΡΟΣ, ΡΥΘΜΙΣΗ, Υ, ΤΗΡΩ, ΜΑΙΟΣ, ΑΥΛΟ"
+    assert Unicode.String.upcase(string, locale: :el) == upcased
+  end
+
+  test "Casing dottied I in the Turkish and Azeri languages" do
     assert Unicode.String.Case.Mapping.upcase("ii", :tr) == "İİ"
     assert Unicode.String.Case.Mapping.upcase("ii", :az) == "İİ"
     assert Unicode.String.Case.Mapping.upcase("ii") == "II"
+    assert Unicode.String.upcase("Diyarbakır", locale: :tr) == "DİYARBAKIR"
+    assert Unicode.String.downcase("DİYARBAKIR", locale: :tr) == "diyarbakır"
   end
 
   test "Titlecasing in Dutch for leading `ij` dipthong" do
@@ -41,11 +49,5 @@ defmodule UnicodeString.Casing.Test do
     assert Unicode.String.casing_locale(:lt) == {:ok, :lt}
     assert Unicode.String.casing_locale(:en) == {:ok, :any}
     assert Unicode.String.casing_locale(:ar) == {:ok, :any}
-  end
-
-  test "That accents are removed when upcasing Greek" do
-    string = "Πατάτα, Αέρας, Μυστήριο, Ωραίο, Μαΐου, Πόρος, Ρύθμιση, ΰ, Τηρώ, Μάιος, άυλο"
-    upcased = "ΠΑΤΑΤΑ, ΑΕΡΑΣ, ΜΥΣΤΗΡΙΟ, ΩΡΑΙΟ, ΜΑΙΟΥ, ΠΟΡΟΣ, ΡΥΘΜΙΣΗ, Υ, ΤΗΡΩ, ΜΑΙΟΣ, ΑΥΛΟ"
-    assert Unicode.String.upcase(string, locale: :el) == upcased
   end
 end
